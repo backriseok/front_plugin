a9f7bfb7907f886f1099b7fda9a4cee9ea343aa0
backriseok <122763998+backriseok@users.noreply.github.com>
2023-01-24 08:08:22 +0100

    Add files via upload

---
+++ popup.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * -------------------------------------------------------------------------
+ * Example plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Example.
+ *
+ * Example is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Example is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Example. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2006-2022 by Example plugin team.
+ * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/example
+ * -------------------------------------------------------------------------
+ */
+
+include ('../../../inc/includes.php');
+
+Session::checkLoginUser();
+
+if (isset($_GET["popup"])) {
+   $_SESSION["glpipopup"]["name"] = $_GET["popup"];
+}
+
+if (isset($_SESSION["glpipopup"]["name"])) {
+   switch ($_SESSION["glpipopup"]["name"]) {
+      case "test_rule" :
+         Html::popHeader(__('Test'), $_SERVER['PHP_SELF']);
+         include "../../../front/rule.test.php";
+         break;
+
+      case "test_all_rules" :
+         Html::popHeader(__('Test rules engine'), $_SERVER['PHP_SELF']);
+         include "../../../front/rulesengine.test.php";
+         break;
+
+      case "show_cache" :
+         Html::popHeader(__('Cache information'), $_SERVER['PHP_SELF']);
+         include "../../../front/rule.cache.php";
+         break;
+   }
+   echo "<div class='center'><br><a href='javascript:window.close()'>".__('Back')."</a>";
+   echo "</div>";
+   Html::popFooter();
+}
